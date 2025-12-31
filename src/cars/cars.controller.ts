a89902492ebc165a14624cbaeec065b307d599cb
Carlos Galindo <utm22030587@utma.edu.mx>
2025-12-31 16:48:40 -0600

    refactor: use validation pipe globally

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -7,15 +7,12 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
-  UsePipes,
-  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove no expected params
+      forbidNonWhitelisted: true, // Throw an error is no expected params are received
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

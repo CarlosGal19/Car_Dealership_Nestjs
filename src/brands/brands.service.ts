438749042141ce0de81d495ba3d3a6e23fdea85c
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-01 12:40:08 -0600

    feat: add brands crud as resource

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,69 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Chevrolet',
+      createdAt: Date.now(),
+    },
+    {
+      id: uuid(),
+      name: 'BYD',
+      createdAt: Date.now(),
+    },
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const id = uuid();
+    const createdAt = Date.now();
+
+    const brandToAdd = { ...createBrandDto, id, createdAt };
+
+    this.brands.push(brandToAdd);
+
+    return { id };
+  }
+
+  findAll() {
+    return { brands: this.brands };
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) {
+      throw new NotFoundException(`The brand with id ${id} does not exist`);
+    }
+    return { brand };
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    this.findOne(id);
+
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        return {
+          id: brand.id,
+          name: updateBrandDto.name || brand.name,
+          createdAt: brand.createdAt,
+          updatedAt: Date.now(),
+        };
+      }
+      return brand;
+    });
+
+    return { id };
+  }
+
+  remove(id: string) {
+    this.findOne(id);
+    this.brands = this.brands.filter((brand) => brand.id !== id);
+    return { id };
+  }
+}
